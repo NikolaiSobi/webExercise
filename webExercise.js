dbf2f4f9c2d3687ff7486e847ed5d866132c8fdd
NikolaiSobi <nikolaisobioch@gmail.com>
2023-07-22 01:52:29 -0600

    first commit

---
+++ webExercise.js
@@ -0,0 +1,40 @@
+/*
+## **Part One: Solidify Terminology**
+
+In your own terms, define the following terms:
+
+- What is HTTP?
+HyperText Transfer Protocol is a set of rules on how to communicate between a browser and a web server.
+
+- What is a URL?
+A Uniform Resource Locator is a web address.
+
+- What is DNS?
+The Domain Name System is essentially the phonebook of the internet that converts hostnames into ip addresses.
+
+- What is a query string?
+The query string provides extra information as part of the URL.
+
+- What are two HTTP verbs and how are they different?
+GET requests data from specific place and POST is used to send data to a server which it makes or changes.
+
+- What is an HTTP request?
+A request is sent to get access to a resource on the server.
+
+- What is an HTTP response?
+A response is the answer from the server to the request.
+
+- What is an HTTP header? Give a couple examples of request and response headers you have seen.
+HTTP header transmits more information during the requests and responses.
+HTTP request header = Accept-, Content-Type
+HTTP response header = Age, Location, or Server
+
+- What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+Browser translates hostname to ip address.
+A request is made using HTTP headers.
+Server sends back HTTP response.
+Browser then renders HTML
+
+
+
+*/
